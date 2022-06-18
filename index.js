7ffc062811948c189fe39c341ec3a3a89b6c6631
Alan ROGER <aa.roger404@gmail.com>
2022-06-18 15:50:51 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a, b) { return a - b })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) return this.items[pos]
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.length < 1) throw new Error('EmptySortedList');
+    return this.items.at(-1)  // .at(-1) <=> this.length-1 <=> dernier element du tableau
+  }
 
-  min() {}
+  min() {
+    if (this.length < 1) throw new Error('EmptySortedList');
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if (this.length < 1) return 0
+    return this.items.reduce(function (a, b) { return a + b })
+  }
 
-  avg() {}
+  avg() {
+    if (this.length < 1) throw new Error('EmptySortedList'); // STOP
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
